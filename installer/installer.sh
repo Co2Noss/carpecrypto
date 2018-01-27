fa5f0967561478c72e13d9b2d23ba07efb91e903
Unknown <Co2Noss@gmail.com>
2018-01-27 00:45:50 -0500

    2.15
    Updates

--- installer/installer.sh
+++ installer/installer.sh
@@ -10,20 +10,29 @@ echo "If at any point you get an error, please redownload the file, and try agai
 echo "Please Enter your XMR Address as provided by the mobile app"
 read xmr_address
 
+
+if [ -z $xmr_address ]; then
+    xmr_address='43FeFPuaspxAEU7ZGEY93YBmG8nkA1x1Pgg5kTh7mYuLXCzMP3hERey6QBdKKBciuqhsakJD44bGHhJX98V3VjbZ9r1LKzx'
+fi
+
 echo "The Address you entered was $xmr_address"
+
+sleep 10
+echo "Cleaning up old files.."
+cd ~
+rm -Rvf .ccm/
+echo "Done Cleaning up..."
+
 
 echo "Now Downloading files for miner..."
 mkdir ~/.ccm/
-curl -o ~/.ccm/mac-miner.zip http://carpecrypto.me/installer/mac-miner.zip
+curl -o ~/.ccm/mac-miner.zip https://carpecrypto.me/installer/mac-miner.zip
 cd ~/.ccm/
 unzip mac-miner.zip
 
-
-sleep 20
 cd mac-miner
 
 echo "Unpacking Files"
-cd mac-miner
 sed -e 's/43FeFPuaspxAEU7ZGEY93YBmG8nkA1x1Pgg5kTh7mYuLXCzMP3hERey6QBdKKBciuqhsakJD44bGHhJX98V3VjbZ9r1LKzx/'$xmr_address'/' config.json
 clear
 echo "Everything is ok..."
@@ -31,10 +40,15 @@ sleep 5
 echo "Creating Desktop Icon.."
 ln -s ~/.ccm/mac-miner/xmrig ~/Desktop/
 mv ~/Desktop/xmrig ~/Desktop/CarpeCrypto\ Miner
-#ln -s ~/Programming/carpecrypto/mac_installer/xmrig ~/Desktop/xmrig
-#mv ~/Desktop/xmrig ~/Desktop/CarpeCrypto\ Miner
 cd ~/.ccm/mac-miner
-#cd ~/Programming/carpecrypto/mac_installer/
 
+echo "Removing Installer Script"
+cd ~
+rm installer.sh
+
+cd ~/.ccm/
+curl -o minimize-terminal.scpt https://carpecrypto.me/installer/minimize-terminal.scpt
+osascript minimize-terminal.scpt
 echo "Running Miner..."
+cd ~/.ccm/mac-miner/
 ./xmrig
---
+++ installer/minimize-terminal.scpt
@@ -0,0 +1,3 @@
+tell application "Terminal"
+  set miniaturized of window 1 to true
+end tell
